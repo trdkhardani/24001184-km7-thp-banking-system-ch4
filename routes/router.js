d1ab6d0e0b1a56ba96671507805c60c2bec47858
Tridiktya Putra <tridiktya@gmail.com>
2024-10-17 17:56:08 +0700

    feat: balance transfer transaction

--- routes/router.js
+++ routes/router.js
@@ -163,4 +163,75 @@ router.get('/api/v1/accounts/:accountId', async (req, res, next) => {
     }
 })
 
+router.post('/api/v1/transactions', async (req, res, next) => {
+    const sourceAccId = Number(req.body.source_account_id);
+    const destAccId = Number(req.body.destination_account_id);
+    const amount = Number(req.body.amount);
+    try {
+        let getSourceAccInfo = await prisma.bank_Account.findUnique({
+            where: {
+                id: sourceAccId,
+            }
+        })
+
+        let getDestAccInfo = await prisma.bank_Account.findUnique({
+            where: {
+                id: destAccId
+            }
+        })
+
+        if(!getSourceAccInfo || !getDestAccInfo){
+            return res.status(409).json({
+                status: 'failed',
+                message: `invalid account id`
+            })
+        } else if(sourceAccId === destAccId){
+            return res.status(409).json({
+                status: 'failed',
+                message: `cannot do transaction between same account`
+            })
+        } else if(amount > getSourceAccInfo.balance){
+            return res.status(409).json({
+                status: 'failed',
+                message: `insufficient balance`
+            })
+        }
+
+        let transaction = await prisma.transaction.create({
+            data: {
+                source_account_id: sourceAccId,
+                destination_account_id: destAccId,
+                amount: amount
+            }
+        })
+
+        let updateSourceAccBalance = await prisma.bank_Account.update({
+            where: {
+                id: sourceAccId
+            }, 
+            data: {
+                balance: getSourceAccInfo.balance - amount
+            }
+        })
+
+        let updateDestAccBalance = await prisma.bank_Account.update({
+            where: {
+                id: destAccId
+            }, 
+            data: {
+                balance: Number(getDestAccInfo.balance) + Number(amount)
+            }
+        })
+
+        res.json({ // for debugging, will delete later
+            status: 'success',
+            transaction: transaction,
+            source_account: updateSourceAccBalance,
+            destination_account: updateDestAccBalance
+        }) 
+    } catch(err) {
+        next(err)
+    }
+})
+
 export default router;
