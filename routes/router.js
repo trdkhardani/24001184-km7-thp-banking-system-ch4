18d78f08cab33fabc7039682b738e464e2e1ea2b
Tridiktya Putra <tridiktya@gmail.com>
2024-10-18 09:35:02 +0700

    refactor: rename userOps, accountOps, and transactionOps to controllers

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,12 @@
-import userOps from '../controllers/user.js';
-import accountOps from '../controllers/account.js';
-import transactionOps from '../controllers/transaction.js';
+import userController from '../controllers/user.js';
+import accountController from '../controllers/account.js';
+import transactionController from '../controllers/transaction.js';
 
 import express from 'express';
 const app = express();
 
-app.use('/api/v1/users', userOps);
-app.use('/api/v1/accounts', accountOps);
-app.use('/api/v1/transactions', transactionOps);
+app.use('/api/v1/users', userController);
+app.use('/api/v1/accounts', accountController);
+app.use('/api/v1/transactions', transactionController);
 
 export default app;
