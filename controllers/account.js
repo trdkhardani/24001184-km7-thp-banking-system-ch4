58e594f8326f7ae7f8fe7cc008de25dc8a512a15
Tridiktya Putra <tridiktya@gmail.com>
2024-10-18 10:30:04 +0700

    chore: create account validation

--- controllers/account.js
+++ controllers/account.js
@@ -4,28 +4,59 @@ const router = Router();
 import { PrismaClient, Prisma } from '@prisma/client'
 const prisma = new PrismaClient();
 
+import validateAccount from '../validation/account.js';
+
 router.post('/', async (req, res, next) => {
-    const userId = Number(req.body.user_id)
-    try {
-        let getUserId = await prisma.user.findUnique({
-            where: {
-                id: userId
-            }
+    const validatedData = {
+        user_id: Number(req.body.user_id),
+        bank_name: req.body.bank_name,
+        bank_account_number: req.body.bank_account_number,
+    };
+
+    const response = validateAccount(validatedData)
+
+    const balance = Number(req.body.balance);
+
+    if(response.error){
+        return res.status(400).send(response.error.details)
+    } else if(isNaN(balance) || balance < 0){
+        return res.status(400).json({
+            status: 'failed',
+            message: 'Balance must be a positive number'
         })
+    }
 
-        if(!getUserId){
-            return res.status(409).json({
-                status: 'failed',
-                message: `No user with user_id ${req.body.user_id}`
-            })
+    let getUserId = await prisma.user.findUnique({
+        where: {
+            id: validatedData.user_id
         }
+    })
 
+    let existingAccNumber = await prisma.bank_Account.findUnique({
+        where: {
+            bank_account_number: validatedData.bank_account_number
+        }
+    })
+
+    if(!getUserId){
+        return res.status(409).json({
+            status: 'failed',
+            message: `No user with user_id ${req.body.user_id}`
+        })
+    } else if(existingAccNumber){
+        return res.status(409).json({
+            status: 'failed',
+            message: `Bank account number ${validatedData.bank_account_number} has already taken`
+        })
+    }
+
+    try {
         let account = await prisma.bank_Account.create({
             data: {
-                user_id: userId,
-                bank_name: req.body.bank_name,
-                bank_account_number: req.body.bank_account_number,
-                balance: req.body.balance
+                user_id: validatedData.user_id,
+                bank_name: validatedData.bank_name,
+                bank_account_number: validatedData.bank_account_number,
+                balance: balance
             }
         })
         
